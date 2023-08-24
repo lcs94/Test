e661dc8da56ea4f5b8e664b525f943560ffcdd31
lcs94 <changsu_lee@naver.com>
2023-08-24 18:46:51 +0900

    실행위치 변경

--- terraform/modules/CreateWindows10VM.tf
+++ terraform/modules/CreateWindows10VM.tf
@@ -1,23 +1,23 @@
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "azurecliVM"
+resource "azurerm_windows_virtual_machine" "VM" {
+  name                = var.vm_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  size                = "Standard_DS2_v2"
+  size                = var.vm_size
 
   network_interface_ids = [
     "/subscriptions/f85dce05-6791-4464-afde-6ac8649b9b3d/resourceGroups/Automated_Test/providers/Microsoft.Network/networkInterfaces/Automated_Test",
   ]
 
   os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
+    caching              = var.os_disk_caching
+    storage_account_type = var.os_disk_storage_type
   }
 
   source_image_reference {
-    publisher = "MicrosoftWindowsDesktop"
-    offer     = "Windows-10"
-    sku       = "20h2-evd"
-    version   = "latest"
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.version
   }
 
   admin_username = var.admin_username
--- terraform/modules/variables.tf
+++ terraform/modules/variables.tf
@@ -17,3 +17,43 @@ variable "admin_password" {
   description = "The admin password for the virtual machine."
   default = "Tester1234$"
 }
+
+variable "vm_name" {
+  description = "The name of the virtual machine."
+  default     = "azurecliVM"
+}
+
+variable "vm_size" {
+  description = "The size of the virtual machine."
+  default     = "Standard_DS2_v2"
+}
+
+variable "publisher" {
+  description = "The publisher of the Windows image."
+  default     = "MicrosoftWindowsDesktop"
+}
+
+variable "offer" {
+  description = "The offer of the Windows image."
+  default     = "Windows-10"
+}
+
+variable "sku" {
+  description = "The SKU of the Windows image."
+  default     = "20h2-evd"
+}
+
+variable "version" {
+  description = "The version of the Windows image."
+  default     = "latest"
+}
+
+variable "os_disk_caching" {
+  description = "The caching option for the OS disk."
+  default     = "ReadWrite"
+}
+
+variable "os_disk_storage_type" {
+  description = "The storage account type for the OS disk."
+  default     = "Premium_LRS"
+}
