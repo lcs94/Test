3269228488ffc1bf7c9bb5da0a7e104787ab00bb
lcs94 <changsu_lee@naver.com>
2023-08-24 19:00:00 +0900

    type 추가

--- terraform/modules/variables.tf
+++ terraform/modules/variables.tf
@@ -1,59 +1,71 @@
 variable "resource_group_name" {
   description = "The name of the resource group."
-  default = "Automated_Test"
+  default     = "Automated_Test"
+  type        = string
 }
 
 variable "location" {
   description = "The location for the virtual machine."
-  default = "koreacentral"
+  default     = "koreacentral"
+  type        = string
 }
 
 variable "admin_username" {
   description = "The admin username for the virtual machine."
-  default = "Tester"
+  default     = "Tester"
+  type        = string
 }
 
 variable "admin_password" {
   description = "The admin password for the virtual machine."
-  default = "Tester1234$"
+  default     = "Tester1234$"
+  type        = string
 }
 
 variable "vm_name" {
   description = "The name of the virtual machine."
   default     = "azurecliVM"
+  type        = string
 }
 
 variable "vm_size" {
   description = "The size of the virtual machine."
   default     = "Standard_DS2_v2"
+  type        = string
 }
 
 variable "publisher" {
   description = "The publisher of the Windows image."
   default     = "MicrosoftWindowsDesktop"
+  type        = string
 }
 
 variable "offer" {
   description = "The offer of the Windows image."
   default     = "Windows-10"
+  type        = string
 }
 
 variable "sku" {
   description = "The SKU of the Windows image."
   default     = "20h2-evd"
+  type        = string
 }
 
 variable "version" {
   description = "The version of the Windows image."
   default     = "latest"
+  type        = string
 }
 
 variable "os_disk_caching" {
   description = "The caching option for the OS disk."
   default     = "ReadWrite"
+  type        = string
 }
 
 variable "os_disk_storage_type" {
   description = "The storage account type for the OS disk."
   default     = "Premium_LRS"
+  type        = string
 }
