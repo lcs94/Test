466455d94085216bd5279564c95567c264ee3e1f
lcs94 <changsu_lee@naver.com>
2023-08-28 15:00:16 +0900

    azurerm_virtual_network 추가

--- terraform/CreateWindows10VM.tf
+++ terraform/CreateWindows10VM.tf
@@ -29,6 +29,13 @@ resource "random_integer" "default" {
   max   = 9999
 }
 
+resource "azurerm_virtual_network" "default" {
+  name                = "default-vnet"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  address_space       = ["10.0.0.0/16"]
+}
+
 resource "azurerm_windows_virtual_machine" "default" {
   count                = var.subnet_count
   name                 = "vm-${count.index}"
