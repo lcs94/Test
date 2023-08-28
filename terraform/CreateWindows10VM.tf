4e9924cb981e7005f728926483cad6ddb45706f1
lcs94 <changsu_lee@naver.com>
2023-08-28 15:44:09 +0900

    azurerm_virtual_network name 중복 방지

--- terraform/CreateWindows10VM.tf
+++ terraform/CreateWindows10VM.tf
@@ -30,7 +30,7 @@ resource "random_integer" "default" {
 }
 
 resource "azurerm_virtual_network" "default" {
-  name                = "default-vnet"
+  name                = "vm-${random_integer.default[count.index].result}"
   resource_group_name = var.resource_group_name
   location            = var.location
   address_space       = ["10.0.0.0/16"]
