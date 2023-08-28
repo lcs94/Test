ef45fdc80145ca8e16f84bc82c83408ce0cebe72
lcs94 <changsu_lee@naver.com>
2023-08-28 17:16:39 +0900

    test

--- terraform/CreateWindows10VM.tf
+++ terraform/CreateWindows10VM.tf
@@ -1,3 +1,9 @@
+resource "azurerm_public_ip" "default" {
+  name                = "ElasticIP"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+}
 resource "azurerm_subnet" "default" {
   count                = var.subnet_count
   name                 = "subnet-${random_integer.default[count.index].result}"
@@ -16,6 +22,8 @@ resource "azurerm_network_interface" "default" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.default[count.index].id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id           = azurerm_public_ip.default.id
+
   }
 
   tags = {
