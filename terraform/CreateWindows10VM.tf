67d387453a52d7ec09d6250e5e6ef07cc7135b75
lcs94 <changsu_lee@naver.com>
2023-08-28 15:47:36 +0900

    count 참조오류

--- terraform/CreateWindows10VM.tf
+++ terraform/CreateWindows10VM.tf
@@ -30,7 +30,8 @@ resource "random_integer" "default" {
 }
 
 resource "azurerm_virtual_network" "default" {
-  name                = "vm-${random_integer.default[count.index].result}"
+  count                = var.subnet_count
+  name                = "vnet-${random_integer.default[count.index].result}"
   resource_group_name = var.resource_group_name
   location            = var.location
   address_space       = ["10.0.0.0/16"]
