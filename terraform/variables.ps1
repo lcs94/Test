4c3c9b24299b4a89dd79fc15996aaea87af1bd70
lcs94 <changsu_lee@naver.com>
2023-09-04 22:11:46 +0900

    test

--- terraform/variables.ps1
+++ terraform/variables.ps1
@@ -5,8 +5,11 @@ $jsonData = @{
     "TEST" = "$(TEST)"
 } | ConvertTo-Json
 
-$jsonData | Set-Content -Path "variables_result.json"
-
-$variablesResult = Get-Content -Raw "variables_result.json"
-
-Write-Host "JSON Data: $variablesResult"
+try {
+    $jsonData | Set-Content -Path "variables_result.json"
+    $variablesResult = Get-Content -Raw "variables_result.json"
+    Write-Host "JSON Data: $variablesResult"
+} catch {
+    Write-Host "Error: $_"  # 오류 메시지 출력
+    exit 1  # 오류가 발생하면 스크립트를 종료하고 오류 코드 1을 반환
+}
