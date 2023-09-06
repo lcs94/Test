49993512820c808307d731bf126d4e5539e5db7b
lcs94 <changsu_lee@naver.com>
2023-09-06 14:32:44 +0900

    test

--- terraform/CreateWindows10VM.tf
+++ terraform/CreateWindows10VM.tf
@@ -98,7 +98,7 @@ resource "azurerm_windows_virtual_machine" "default" {
       type     = "winrm"
       user     = "var.admin_username"
       password = "var.admin_password"
-      host     = azurerm_public_ip.default[count.index].ip_address
+      host     = azurerm_windows_virtual_machine.default[count.index].private_ip_address
     }
   }
 
